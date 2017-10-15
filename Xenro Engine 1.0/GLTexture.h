ab1c7022a07be9ef4abc3920266df7f65b29a885
Joseph Miller <fireyshyguy@gmail.com>
2017-10-14 19:21:58 -0700

    Deleted Old unused files.

--- Xenro Engine 1.0/GLTexture.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<GL\glew.h>
-
-struct GLTexture {
-	GLuint ID;
-	int height;
-	int width;
-};
--- Xenro Engine 1.0/ResourceManager.cpp
+++
@@ -1,7 +0,0 @@
-#include "ResourceManager.h"
-
-TextureCache ResourceManager::m_texturecache;
- 
-GLTexture ResourceManager::getTexture(std::string texturePath) {
-	return m_texturecache.getTexture(texturePath);
-}
