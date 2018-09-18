cbc57a11e746fae8af531fcb3617e9ae7d49d4c4
jmiller7023 <fireyshyguy@gmail.com>
2018-09-17 23:47:53 -0700

    Hide build files

--- Burning_Sabyr/StatSet.cpp
+++
@@ -1,8 +0,0 @@
-#include "Statset.h"
-
-Statset::Statset(int HP, int MoveNum, int Dext, int Strength, int Wisdom, int Intelligence, int Level, int EXP) 
-	:hp(HP), moveNum(MoveNum), dext(Dext), strength(Strength), wisdom(Wisdom),
-	intelligence(Intelligence), level(Level), exp(EXP)
-{
-	//Empty
-}
